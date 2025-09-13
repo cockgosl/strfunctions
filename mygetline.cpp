17c8bd80f2db6678ed2934f16614ef6a8e7a4399
cockgosl <Igra.nikolaev@gmail.com>
2025-09-14 00:10:08 +0300

    Add files via upload
    mygetline doesn't work

--- mygetline.cpp
+++ mygetline.cpp
@@ -1,8 +1,14 @@
-#include <mygetline.h>
+#include "mygetline.h"
 
 size_t mygetline (char** line, size_t* size, FILE* stream) {
-    *line = malloc (*size + 1);
-    printf ("print the string: ");
-    fgets (*line, size, stream);
-    return (strlen(*line));
+    *line = (char*)malloc (*size + 1);
+    for (int i = 0; (*line)[i] != '\0' && (*line)[i] != EOF && i < *size ; i++) {
+        printf ("%c", (*line)[i]);
+        (*line)[i] = fgetc (stream);
+        if (i == *size) {
+            *(line)[i] = '\0';
+            return strlen(*line);
+        }
+    }
+    return strlen (*line);
 }
--- mygetline.h
+++ mygetline.h
@@ -1,4 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 size_t mygetline (char** line, size_t* size, FILE* stream);
